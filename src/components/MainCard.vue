90bcf6e31141515c346147fe55b3101b413506e0
tritiumm <juebanmonage@163.com>
2023-04-11 21:05:42 +0800

    feat: hospital upload

---
+++ src/components/MainCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { NDivider } from 'naive-ui';
+defineProps<{
+    title: string
+}>();
+</script>
+<template>
+    <div class="demo-card">
+        <div class="demo-card--head">{{ title }}</div>
+        <slot name="top"></slot>
+        <NDivider></NDivider>
+        <slot name="bottom"></slot>
+    </div>
+</template>
+<style lang="less" scoped>
+.demo-card  {
+    width: 800px;
+    padding: 10px 30px;
+    border-radius: 20px;
+    box-shadow: rgba(0, 0, 0, 0.1) 2px 2px 6px 4px;
+    &--head {
+        text-align: center;
+        font-size: 20px;
+        font-weight: bold;
+    }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HospitalEnd from '../views/HospitalEnd.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,11 +7,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HospitalEnd
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/company',
+      name: 'company',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
